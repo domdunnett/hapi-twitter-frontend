5a96e255487068b78055016b125d0ceb60205861
Dominic Michael Joshua Dunnett <domdunnett@gmail.com>
2015-04-18 22:38:33 +0800

    add post functionality.list all tweets and search ajax requests

--- js/application.js
+++ js/application.js
@@ -3,44 +3,65 @@ $(document).ready(function() {
 //	Changing background
 
 
-	var content = $('body');
-	
-	var backgrounds = [
-		'url(images/background-1.jpg)',
-		'url(images/background-2.jpg)',
-		'url(images/background-3.jpg)',
-		'url(images/background-4.jpg)'
-	];
+//	var content = $('body');
+//	
+//	var backgrounds = [
+//		'url(images/background-1.jpg)',
+//		'url(images/background-2.jpg)',
+//		'url(images/background-3.jpg)',
+//		'url(images/background-4.jpg)'
+//	];
+//
+//	content.css('background-image', backgrounds[0]);
+//
+//	function changeBackground() {
+//		var randomIndex = Math.floor(Math.random()*backgrounds.length);
+//		content.animate({ 'background-image': backgrounds[randomIndex] }, 500);
+//	}
+//	
+//	setInterval(changeBackground, 500);
 
-	content.css('background-image', backgrounds[0]);
-
-	function changeBackground() {
-		var randomIndex = Math.floor(Math.random()*backgrounds.length);
-		content.animate({ 'background-image': backgrounds[randomIndex] }, 500);
-	}
+	$('#post-tweet').hide();
 	
-	setInterval(changeBackground, 500);
-
-	
-	
-//	Ajax Request template
+	//	Ajax Request template
 	function Request() {
-   this.type = '';
-   this.url = 'http://localhost:3000';
-   this.data = {};
-   this.dataType = 'json';
-   this.success = function(response) {
-   };
+		this.type = '';
+		this.url = 'http://localhost:3000';
+		this.data = {};
+		this.xhrFields = { withCredentials: true };
+		this.dataType = 'json';
+		this.success = function(response) {
+		};
 	}
 	
+	//List all tweets
+	var allTweetsRequest = new Request();
+	allTweetsRequest.type = 'GET';
+	allTweetsRequest.url += '/tweets';
+	allTweetsRequest.success = function(response) {
+		for(var i = 0; i < response.length; i++) {
+			var text= '';
+			text += '<li>' + response[i].tweet + '</li>';
+			text += '<li class="tweet-user"><em>' + response[i].user + '</em></li>';
+			
+			$('#all-tweets').append(text);
+		}
+	};
+	allTweetsRequest.error = function(response) {
+		$('#all-tweets').append('<li>' + response.responseText + '</li>');
+	};
+
+
+	$.ajax(allTweetsRequest);
+
 	
-// Prevent closing the sign up form on click
+	// Prevent closing the sign up form on click
 	$('.dropdown-menu').click(function(e) {
 		 e.stopPropagation();
 	});
 	
 	
-//sign up Request
+	//sign up Request
 	$('#signup').on('click', function() {
 		
 		var usernameInput = $('#username').val();
@@ -59,8 +80,9 @@ $(document).ready(function() {
 		newRequest.url += '/users';
 		newRequest.data = dataPackage;
 		newRequest.success = function(response) {
-			$('#signup-dropdown').hide();
-			$('#signin-dropdown').hide();
+			$('#signup-dropdown').hide('slow');
+			$('#signin-dropdown').hide('slow');
+			$('#post-tweet').show('slow');
 			console.log(response);
 		};
 		newRequest.error = function(response) {
@@ -73,7 +95,7 @@ $(document).ready(function() {
 
 	});
 	
-//sign in request
+	//sign in request
 	$('#signin').on('click', function() {
 		
 		var usernameInput = $('#signin-username').val();
@@ -89,10 +111,10 @@ $(document).ready(function() {
 		newRequest.type = 'POST';
 		newRequest.url += '/sessions';
 		newRequest.data = dataPackage;
-		newRequest.xhrFields = { withCredentials: true };
 		newRequest.success = function(response) {
-			$('#signin-dropdown').hide();
-			$('#signup-dropdown').hide();
+			$('#signin-dropdown').hide('slow');
+			$('#signup-dropdown').hide('slow');
+			$('#post-tweet').show('slow');
 			$('#signedin').text("Hello " + usernameInput + ", you are now signed in.")
 			console.log(response);
 		};
@@ -106,7 +128,69 @@ $(document).ready(function() {
 
 	});
 	
+	// Search tweets request
+	$('#search-button').on('click', function() {
+		$('#all-tweets').html('');
+		
+		var searchInput = $('#user-search').val();
+		
+		var searchRequest = new Request();
+		searchRequest.type = 'GET';
+		searchRequest.url += '/tweets/search/' + searchInput;
+		searchRequest.success = function(response) {
+			console.log(response);
+			
+			
+			for(var i = 0; i < response.length; i++) {
+				var text= '';
+				text += '<li>' + response[i].tweet + '</li>';
+				text += '<li class="tweet-user"><em>' + response[i].user + '</em></li>';
+
+				$('#all-tweets').append(text);
+				
+			}
+		}
+		console.log(searchRequest);
+		
+		$.ajax(searchRequest);
+	});
 	
+	//Post a tweet
+	$('#new-tweet').on('click', function() {
+
+		var tweetInput = $('#tweet-input').val();
+		var newTweetRequest = new Request();
+		var dataPackage = {
+			tweet: {
+				tweet: tweetInput,
+			}
+		};
+
+		newTweetRequest.type = 'POST';
+		newTweetRequest.url += '/tweets';
+		newTweetRequest.data = dataPackage;
+		newTweetRequest.success = function(response) {
+			$('#newtweet').after('<span>Succesfully Posted</span>').hide(500, function() {
+				$(this).remove();
+			});
+		};
+		newTweetRequest.error = function(response) {
+			$('#tweet-input').attr('class', 'has-error');
+			console.log(response);
+		};
+
+
+		$.ajax(newTweetRequest);
+
+	});
+	
+	
+//sign out request
+//	$('#signout').on('click', function() {
+//		
+//	}
+//	
+//Home page reload link
 	$('.navbar-brand').on('click', function() {
 		location.reload();
 	});
